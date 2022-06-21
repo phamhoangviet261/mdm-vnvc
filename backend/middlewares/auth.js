aec12426fbee2c2cc910dc3bdebd17c747424d21
LAP12400\LAP12400-local <hoangviet.it.hcmus@gmail.com>
2022-06-21 14:56:30 +0700

    Clone env to txt, create PackageVaccine model

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,7 @@
 const checkLogin = (req, res, next) => {
     console.log('================================================================');
     console.log('START_CHECK_AUTHENTICATION');
+    console.log({res: res.cookie});
     console.log('END_CHECK_AUTHENTICATION');
     console.log('================================================================');
     next();
