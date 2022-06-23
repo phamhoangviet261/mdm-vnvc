d00562f5ef5e4c2cf2e829b882b2fd7e1950d066
LAP12400\LAP12400-local <hoangviet.it.hcmus@gmail.com>
2022-06-23 14:57:10 +0700

    add route package + more

--- backend/routes/packageVaccine.js
+++ backend/routes/packageVaccine.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-const PackageVaccine = require('../models/PackageVaccine')
+const PackageVaccine = require('../models/PackageVaccine');
+const Vaccine = require('../models/Vaccine');
 const mongoose = require('mongoose')
 
 router.get('/', async (req, res, next) => {
@@ -19,7 +20,36 @@ router.get('/:packageId', async (req, res, next) => {
     try {
         console.log("req.params.packageId: ", req.params.packageId);
         const packages = await PackageVaccine.find({id: req.params.packageId});
+        console.log({package: packages});
+        console.log({package: packages[0]?.vaccines});
+        if(packages[0]?.vaccines.length>0) {
+            for (const vc in packages[0].vaccines) {
+                console.log({vc});
+                const vaccineDetails = await Vaccine.find({id: packages[0]?.vaccines[vc].vcid});
+                console.log({vaccineDetails});
+                packages[0].vaccines[vc].vaccineDetails = vaccineDetails;
+            }
+        }
+        return res.status(200).json({data: packages});
+    } catch (errors) {
+        console.log(errors);
+    }
+    
+})
+
+router.get('/target/:targetId', async (req, res, next) => {
+    try {
+        console.log("req.params.targetId: ", req.params.targetId);
+        const packages = await PackageVaccine.find({"target.targetId": req.params.targetId});
         console.log({vcc: packages});
+        if(packages[0]?.vaccines.length>0) {
+            for (const vc in packages[0].vaccines) {
+                console.log({vc});
+                const vaccineDetails = await Vaccine.find({id: packages[0]?.vaccines[vc].vcid});
+                console.log({vaccineDetails});
+                packages[0].vaccines[vc].vaccineDetails = vaccineDetails;
+            }
+        }
         return res.status(200).json({data: packages});
     } catch (errors) {
         console.log(errors);
