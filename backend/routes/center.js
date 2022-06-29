2b898bf23ed6572d4f141931d48a7bda2282432c
LAP12400\LAP12400-local <hoangviet.it.hcmus@gmail.com>
2022-06-29 13:33:17 +0700

    Add city and ward to get list center API

--- backend/routes/center.js
+++ backend/routes/center.js
@@ -7,7 +7,13 @@ const mongoose = require('mongoose')
 router.get('/', async (req, res, next) => {
     try {
         const centers = await Center.find();
-        return res.status(200).json({data: centers});
+        let fakeCenter = JSON.parse(JSON.stringify(centers));
+        for (let i = 0; i < fakeCenter.length; i++) {
+            fakeCenter[i].centerArr = fakeCenter[i].addressDetail.split(',').map(element => element.trim());
+            fakeCenter[i].city  = fakeCenter[i].centerArr[fakeCenter[i].centerArr.length-1]
+            fakeCenter[i].ward  = fakeCenter[i].centerArr[fakeCenter[i].centerArr.length-2]
+        }
+        return res.status(200).json({data: fakeCenter});
     } catch (errors) {
         console.log(errors);
         return res.status(400).json({success: false, message: errors.message});
